486d7140c802a6daf51340e4f0352e5a9e90592b
BRANDON OSMAR OSMAR <1720110318@utectulancingo.edu.mx>
2021-02-06 06:18:32 +0000

    listo

---
+++ Semana2/Distancia.py
@@ -0,0 +1,10 @@
+class lold():
+    def __init__(self):
+        pass
+
+    def numeros_s(self,x1,x2,y1,y2):
+        resultado=((x2-x1)**2 +((y2-y1)**2))
+        print(resultado**.5)
+
+objeto=lold()
+objeto.numeros_s(3.17,4.99,4.78,7.88)
