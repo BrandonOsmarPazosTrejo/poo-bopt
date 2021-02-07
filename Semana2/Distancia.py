ed04ff6c814c1aabe9388ef0b2c8e26294a7fed0
BRANDON OSMAR OSMAR <1720110318@utectulancingo.edu.mx>
2021-02-07 18:54:11 +0000

    terminado

--- Semana2/Distancia.py
+++ Semana2/Distancia.py
@@ -1,10 +1,31 @@
-class lold():
+class Distancia():
     def __init__(self):
         pass
+    print("Distancia")
 
-    def numeros_s(self,x1,x2,y1,y2):
+    def cordenadas1(self,x1,y1,x2,y2):
         resultado=((x2-x1)**2 +((y2-y1)**2))
-        print(resultado**.5)
+        print("RESULTADO",resultado**.5)
+    
+    def cordenadas2(self,x1,y1,x2,y2):
+        resultado=((x2-x1)**2 +((y2-y1)**2))
+        print("RESULTADO",resultado**.5)
 
-objeto=lold()
-objeto.numeros_s(3.17,4.99,4.78,7.88)
+    def cordenadas3(self,x1,y1,x2,y2):
+        resultado=((x2-x1)**2 +((y2-y1)**2))
+        print("RESULTADO",resultado**.5)
+
+    def cordenadas4(self,x1,y1,x2,y2):
+        resultado=((x2-x1)**2 +((y2-y1)**2))
+        print("RESULTADO",resultado**.5)
+
+    def cordenadas5(self,x1,y1,x2,y2):
+        resultado=((x2-x1)**2 +((y2-y1)**2))
+        print("RESULTADO",resultado**.5)
+
+objeto=Distancia()
+objeto.cordenadas1(3.17,4.78,4.99,7.88)
+objeto.cordenadas2(7.15,21.6,1.93,4.38)
+objeto.cordenadas3(12.17,10.4,10.4,29.3)
+objeto.cordenadas4(39.4,78.9,68.3,187.2)
+objeto.cordenadas5(88.7,118.3,295.3,18.4)
